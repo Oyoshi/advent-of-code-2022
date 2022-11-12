8c40c05cb6dbda49cea18e2a44dbecdf22faeb46
Oyoshi <rafal.apriasz@gmail.com>
2022-11-12 15:04:14 +0100

    feat: move code from main to the FlowBuilder

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 import logging
-from args_parser import parse_args
-from tasks_solver import TasksSolver
+from flow_builder import FlowBuilder
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
 
 def main():
-    config = parse_args()
-    tasks_solver = TasksSolver()
-    tasks_solver.solve(config)
+    flow_builder = FlowBuilder()
+    flow_builder.execute_flow()
 
 
 if __name__ == "__main__":
