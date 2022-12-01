769709a408eb16213999be3375b57db938ed4da4
Oyoshi <rafal.apriasz@gmail.com>
2022-12-01 08:38:37 +0100

    refactor: move common code of fining max elements in the list to separated utility function

--- days_solvers/day_01/day_01_solver.py
+++ days_solvers/day_01/day_01_solver.py
@@ -12,17 +12,21 @@ class Day01Solver(DaySolver):
         file_path = os.path.join("inputs", self.get_input_filename())
         input_data = []
         with open(file_path) as f:
-            elf_carries = []
+            calories = []
             for val in f:
                 if val.rstrip().isdigit():
-                    elf_carries.append(int(val))
+                    calories.append(int(val))
                 else:
-                    input_data.append(elf_carries)
-                    elf_carries = []
+                    input_data.append(calories)
+                    calories = []
         return [ft.reduce(lambda a, b: a + b, list_item) for list_item in input_data]
 
     def solve_part_1(self):
-        return sorted(self.input_data)[-1:]
+        return sum_nth_max(self.input_data, 1)
 
     def solve_part_2(self):
-        return ft.reduce(lambda a, b: a + b, sorted(self.input_data)[-3:])
+        return sum_nth_max(self.input_data, 3)
+
+
+def sum_nth_max(iterable_input, nth):
+    return ft.reduce(lambda a, b: a + b, sorted(iterable_input)[-nth:])
