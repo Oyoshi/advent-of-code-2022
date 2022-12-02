7062d0f2389d5533ca8935fe51f556c6102f668d
Oyoshi <rafal.apriasz@gmail.com>
2022-12-02 09:28:33 +0100

    move related UTs to proper TestCase classes

--- days_solvers/day_01/test_day_01_solver.py
+++ days_solvers/day_01/test_day_01_solver.py
@@ -1,17 +1,24 @@
+import unittest
 from .day_01_solver import Day01Solver
 
 
-def load_input_mock():
-    return [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+class Day01SolverTestCase(unittest.TestCase):
+    def setUp(self):
+        self.solver = Day01Solver()
+        self.mock_input_data = [
+            [1000, 2000, 3000],
+            [4000],
+            [5000, 6000],
+            [7000, 8000, 9000],
+            [10000],
+        ]
+        self.solver.load_input = lambda: self.mock_input_data
 
+    def tearDown(self):
+        self.solver = None
 
-def test_solve_part_1():
-    solver = Day01Solver()
-    solver.load_input = load_input_mock
-    assert solver.solve(part=1)["val"] == 24000
+    def test_solve_part_1(self):
+        assert self.solver.solve(part=1)["val"] == 24000
 
-
-def test_solve_part_2():
-    solver = Day01Solver()
-    solver.load_input = load_input_mock
-    assert solver.solve(part=2)["val"] == 45000
+    def test_solve_part_2(self):
+        assert self.solver.solve(part=2)["val"] == 45000
--- days_solvers/day_02/test_day_02_solver.py
+++ days_solvers/day_02/test_day_02_solver.py
@@ -1,17 +1,18 @@
+import unittest
 from .day_02_solver import Day02Solver
 
 
-def load_input_mock():
-    return [["A", "Y"], ["B", "X"], ["C", "Z"]]
+class Day02SolverTestCase(unittest.TestCase):
+    def setUp(self):
+        self.solver = Day02Solver()
+        self.mock_input_data = [["A", "Y"], ["B", "X"], ["C", "Z"]]
+        self.solver.load_input = lambda: self.mock_input_data
 
+    def tearDown(self):
+        self.solver = None
 
-def test_solve_part_1():
-    solver = Day02Solver()
-    solver.load_input = load_input_mock
-    assert solver.solve(part=1)["val"] == 15
+    def test_solve_part_1(self):
+        assert self.solver.solve(part=1)["val"] == 15
 
-
-def test_solve_part_2():
-    solver = Day02Solver()
-    solver.load_input = load_input_mock
-    assert solver.solve(part=2)["val"] == 12
+    def test_solve_part_2(self):
+        assert self.solver.solve(part=2)["val"] == 12
