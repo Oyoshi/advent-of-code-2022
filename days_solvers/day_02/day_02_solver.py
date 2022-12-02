7ce4d46dcde73c2f06062ffe377296e27424b8b4
Oyoshi <rafal.apriasz@gmail.com>
2022-12-02 09:11:59 +0100

    solution for day 2 part 2

--- days_solvers/day_02/day_02_solver.py
+++ days_solvers/day_02/day_02_solver.py
@@ -16,30 +16,60 @@ class Day02Solver(DaySolver):
 
     def solve_part_1(self):
         return ft.reduce(
-            lambda a, b: a + b, [self.score(list_item) for list_item in self.input_data]
+            lambda a, b: a + b,
+            [self.score(list_item, 1) for list_item in self.input_data],
         )
 
     def solve_part_2(self):
-        pass
+        return ft.reduce(
+            lambda a, b: a + b,
+            [self.score(list_item, 2) for list_item in self.input_data],
+        )
 
-    def score(self, item):
+    def score(self, item, part):
         lhs, rhs = item[0], item[1]
-        return self.score_choose(rhs) + self.score_round(lhs, rhs)
+        return self.score_choose(rhs, part) + self.score_round(lhs, rhs, part)
 
-    def score_round(self, lhs, rhs):
-        if (
-            (lhs == "A" and rhs == "X")
-            or (lhs == "B" and rhs == "Y")
-            or (lhs == "C" and rhs == "Z")
-        ):
-            return 3
-        if (
-            (lhs == "A" and rhs == "Y")
-            or (lhs == "B" and rhs == "Z")
-            or (lhs == "C" and rhs == "X")
-        ):
-            return 6
-        return 0
+    def score_round(self, lhs, rhs, part):
+        if part == 1:
+            if (
+                (lhs == "A" and rhs == "X")
+                or (lhs == "B" and rhs == "Y")
+                or (lhs == "C" and rhs == "Z")
+            ):
+                return 3
+            if (
+                (lhs == "A" and rhs == "Y")
+                or (lhs == "B" and rhs == "Z")
+                or (lhs == "C" and rhs == "X")
+            ):
+                return 6
+            return 0
+        if part == 2:
+            if lhs == "A":
+                if rhs == "X":
+                    return 3
+                if rhs == "Y":
+                    return 1
+                if rhs == "Z":
+                    return 2
+            if lhs == "B":
+                if rhs == "X":
+                    return 1
+                if rhs == "Y":
+                    return 2
+                if rhs == "Z":
+                    return 3
+            if lhs == "C":
+                if rhs == "X":
+                    return 2
+                if rhs == "Y":
+                    return 3
+                if rhs == "Z":
+                    return 1
 
-    def score_choose(self, choose):
-        return 1 if choose == "X" else 2 if choose == "Y" else 3
+    def score_choose(self, choose, part):
+        if part == 1:
+            return 1 if choose == "X" else 2 if choose == "Y" else 3
+        if part == 2:
+            return 0 if choose == "X" else 3 if choose == "Y" else 6
--- days_solvers/day_02/test_day_02_solver.py
+++ days_solvers/day_02/test_day_02_solver.py
@@ -9,3 +9,9 @@ def test_solve_part_1():
     solver = Day02Solver()
     solver.load_input = load_input_mock
     assert solver.solve(part=1)["val"] == 15
+
+
+def test_solve_part_2():
+    solver = Day02Solver()
+    solver.load_input = load_input_mock
+    assert solver.solve(part=2)["val"] == 12
