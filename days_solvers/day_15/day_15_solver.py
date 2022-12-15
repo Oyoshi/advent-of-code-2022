9ecc0a73e63cbf491df6867d50f6e59e05286063
Oyoshi <rafal.apriasz@gmail.com>
2022-12-15 20:37:32 +0100

    solution for day 15 part 2

--- days_solvers/day_15/day_15_solver.py
+++ days_solvers/day_15/day_15_solver.py
@@ -6,69 +6,77 @@ class Day15Solver(DaySolver):
         self.day = "15"
 
     def load_input_impl(self, file):
-        return [
-            [
-                tuple(
-                    map(
-                        int,
-                        (
-                            e[2].split("=")[1].rstrip(","),
-                            e[3].split("=")[1].rstrip(":"),
+        return list(
+            map(
+                lambda e: (e[0], e[1], self.compute_distance(e[0], e[1])),
+                [
+                    [
+                        tuple(
+                            map(
+                                int,
+                                (
+                                    e[2].split("=")[1].rstrip(","),
+                                    e[3].split("=")[1].rstrip(":"),
+                                ),
+                            )
                         ),
-                    )
-                ),
-                tuple(map(int, (e[8].split("=")[1].rstrip(","), e[9].split("=")[1]))),
-            ]
-            for e in [line.rstrip().split() for line in file]
-        ]
+                        tuple(
+                            map(
+                                int,
+                                (e[8].split("=")[1].rstrip(","), e[9].split("=")[1]),
+                            )
+                        ),
+                    ]
+                    for e in [line.rstrip().split() for line in file]
+                ],
+            )
+        )
 
     def solve_part_1(self):
         return self.find_no_beacons(2000000)
 
     def solve_part_2(self):
-        # TODO - refactor it asnd add missing UT
-        data = list(
-            map(
-                lambda e: (
-                    e[0][0],
-                    e[0][1],
-                    self.compute_manhattan_distance(e[0], e[1]),
-                ),
-                self.input_data,
-            )
-        )
-        a = (
-            set(x - y + r + 1 for x, y, r in data)
-            .intersection(x - y - r - 1 for x, y, r in data)
-            .pop()
-        )
-        b = (
-            set(x + y + r + 1 for x, y, r in data)
-            .intersection(x + y - r - 1 for x, y, r in data)
-            .pop()
-        )
-        return (a + b) * 4000000 // 2 + (b - a) // 2
+        max_range = 4000000
+        y = 0
+        while y <= max_range:
+            segments = []
+            for sensor, _, dist in self.input_data:
+                delta_h = abs(y - sensor[1])
+                if delta_h <= dist:
+                    segments.append(
+                        [
+                            max(0, sensor[0] - (dist - delta_h)),
+                            min(sensor[0] + (dist - delta_h), max_range + 1),
+                        ]
+                    )
+            sorted_segments = sorted(segments)
+            max_y = sorted_segments[0][1]
+            i = 1
+            while i < len(sorted_segments):
+                max_y = max(max_y, sorted_segments[i - 1][1])
+                if max_y + 1 < sorted_segments[i][0]:
+                    return 4000000 * (sorted_segments[i - 1][1] + 1) + y
+                i += 1
+            y += 1
+        return -1
 
     def find_no_beacons(self, target_y):
         beacons = set(map(lambda e: e[1], self.input_data))
         no_beacons = set()
-        for pair_data in self.input_data:
-            sensor, beacon = pair_data
-            distance = self.compute_manhattan_distance(sensor, beacon)
-            x_min, x_max = sensor[0] - distance, sensor[0] + distance
-            y_min, y_max = sensor[1] - distance, sensor[1] + distance
+        for sensor, _, dist in self.input_data:
+            x_min, x_max = sensor[0] - dist, sensor[0] + dist
+            y_min, y_max = sensor[1] - dist, sensor[1] + dist
             if y_min <= target_y and target_y <= y_max:
                 x = x_min
                 while x <= x_max:
                     potential_empty_pos = (x, target_y)
                     if (
                         potential_empty_pos not in beacons
-                        and self.compute_manhattan_distance(sensor, potential_empty_pos)
-                        <= distance
+                        and self.compute_distance(sensor, potential_empty_pos) <= dist
                     ):
                         no_beacons.add(potential_empty_pos)
                     x += 1
         return len(list(filter(lambda e: e[1] == target_y, no_beacons)))
 
-    def compute_manhattan_distance(self, lhs, rhs):
+    def compute_distance(self, lhs, rhs):
         return abs(lhs[0] - rhs[0]) + abs(lhs[1] - rhs[1])
--- days_solvers/day_15/test_day_15_solver.py
+++ days_solvers/day_15/test_day_15_solver.py
@@ -6,20 +6,20 @@ class Day15SolverTestCase(unittest.TestCase):
     def setUp(self):
         self.solver = Day15Solver()
         mock_input_data = [
-            [(2, 18), (-2, 15)],
-            [(9, 16), (10, 16)],
-            [(13, 2), (15, 3)],
-            [(12, 14), (10, 16)],
-            [(10, 20), (10, 16)],
-            [(14, 17), (10, 16)],
-            [(8, 7), (2, 10)],
-            [(2, 0), (2, 10)],
-            [(0, 11), (2, 10)],
-            [(20, 14), (25, 17)],
-            [(17, 20), (21, 22)],
-            [(16, 7), (15, 3)],
-            [(14, 3), (15, 3)],
-            [(20, 1), (15, 3)],
+            ((2, 18), (-2, 15), 7),
+            ((9, 16), (10, 16), 1),
+            ((13, 2), (15, 3), 3),
+            ((12, 14), (10, 16), 4),
+            ((10, 20), (10, 16), 4),
+            ((14, 17), (10, 16), 5),
+            ((8, 7), (2, 10), 9),
+            ((2, 0), (2, 10), 10),
+            ((0, 11), (2, 10), 3),
+            ((20, 14), (25, 17), 8),
+            ((17, 20), (21, 22), 6),
+            ((16, 7), (15, 3), 5),
+            ((14, 3), (15, 3), 1),
+            ((20, 1), (15, 3), 7),
         ]
         self.solver.load_input = lambda: mock_input_data
         self.solver.solve_part_1 = lambda: self.solver.find_no_beacons(10)
@@ -29,3 +29,6 @@ class Day15SolverTestCase(unittest.TestCase):
 
     def test_solve_part_1(self):
         assert self.solver.solve(part=1)["val"] == 26
+
+    def test_solve_part_2(self):
+        assert self.solver.solve(part=2)["val"] == 56000011
