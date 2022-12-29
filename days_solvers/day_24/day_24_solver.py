49e7535c3f117ade170b7dd85855d977740644c7
Oyoshi <rafal.apriasz@gmail.com>
2022-12-29 11:32:57 +0100

    Solution for Day 24 Part 1 and Part 2

--- days_solvers/day_24/day_24_solver.py
+++ days_solvers/day_24/day_24_solver.py
@@ -1,71 +1,19 @@
+from enum import Enum
+from math import lcm
+import heapq
 from days_solvers import DaySolver
 
-STATE_ELF = 16
-STATE_UP = 8
-STATE_RIGHT = 4
-STATE_DOWN = 2
-STATE_LEFT = 1
 
-parse_map = {
-    "^": STATE_UP,
-    ">": STATE_RIGHT,
-    "v": STATE_DOWN,
-    "<": STATE_LEFT,
-    ".": 0,
-}
-draw_map = {
-    STATE_ELF: "E",
-    STATE_UP: "^",
-    STATE_RIGHT: ">",
-    STATE_DOWN: "v",
-    STATE_LEFT: "<",
-    0: ".",
-}
-neighbors = {
-    (0, -1): STATE_DOWN,
-    (1, 0): STATE_LEFT,
-    (0, 1): STATE_UP,
-    (-1, 0): STATE_RIGHT,
-}
+class Direction(Enum):
+    UP = 1
+    RIGHT = 2
+    DOWN = 3
+    LEFT = 4
 
 
-def draw_scene(generation: dict, dimensions) -> None:
-    width, height = dimensions
-    for y in range(height):
-        for x in range(width):
-            state = generation.get((x, y))
-            if not draw_map.get(state):
-                bliz_num = 0
-                for _ in range(4):
-                    bliz_num += state & 1
-                    state >>= 1
-                print(bliz_num, end="")
-                continue
-            print(draw_map.get(state), end="")
-        print()
-    print()
-
-
-def new_generation(generation: dict, dimensions, start, stop) -> dict:
-    new_generation = {}
-    for cell in generation:
-        new_generation[cell] = 0
-        for offset, state in neighbors.items():
-            check_cell = (cell[0] + offset[0], cell[1] + offset[1])
-            if cell != start and cell != stop:
-                new_generation[cell] |= (
-                    generation.get(
-                        (check_cell[0] % dimensions[0], check_cell[1] % dimensions[1]),
-                        0,
-                    )
-                    & state
-                )
-            new_generation[cell] |= generation.get(check_cell, 0) & STATE_ELF
-        new_generation[cell] |= generation[cell] & STATE_ELF
-        if new_generation[cell] - STATE_ELF > 0:
-            new_generation[cell] ^= STATE_ELF
-
-    return new_generation
+class Tile(Enum):
+    WALL = 1
+    BLIZZARD = 2
 
 
 class Day24Solver(DaySolver):
@@ -73,81 +21,147 @@ class Day24Solver(DaySolver):
         self.day = "24"
 
     def load_input_impl(self, file):
-        return file.read().splitlines()  # [line.rstrip() for line in file]
+        valley_map = {}
+        splitted_input = [list(line.rstrip()) for line in file]
+        for r_idx, row in enumerate(splitted_input):
+            for c_idx, col in enumerate(row):
+                symbol = self.match_symbol(col)
+                if symbol is not None:
+                    coord = (r_idx, c_idx)
+                    valley_map[coord] = symbol
+        height = len(splitted_input)
+        rows = len(splitted_input[0])
+        return valley_map, height, rows
+
+    def match_symbol(self, sym):
+        match sym:
+            case "#":
+                return Tile.WALL
+            case "^":
+                return (Tile.BLIZZARD, Direction.UP)
+            case ">":
+                return (Tile.BLIZZARD, Direction.RIGHT)
+            case "v":
+                return (Tile.BLIZZARD, Direction.DOWN)
+            case "<":
+                return (Tile.BLIZZARD, Direction.LEFT)
+            case _:
+                return None
 
     def solve_part_1(self):
-        input_lines = self.input_data
-        print(input_lines)
-        generation = {
-            (x, y): parse_map.get(char)
-            for y, line in enumerate(input_lines, -1)
-            for x, char in enumerate(line, -1)
-            if not char == "#"
-        }
-        # print(generation)
-
-        width = len(input_lines[0][1:-1])
-        height = len(input_lines[1:-1])
-        # print(width, height)
-        start_pos = (0, -1)
-        end_pos = (width - 1, height)
-
-        generation[start_pos] = STATE_ELF
-        generation[end_pos] = 0
-
-        gen_count = 0
-        while True:
-            # print(gen_count)
-            # draw_scene(generation, (width, height))
-            if generation.get(end_pos) == STATE_ELF:
-                break
-            generation = new_generation(generation, (width, height), start_pos, end_pos)
-            gen_count += 1
-        return gen_count
+        _, height, width = self.input_data
+        start_pos = (0, 1)
+        end_pos = (height - 1, width - 2)
+        positions = [(start_pos, end_pos)]
+        return self.simulate_move(positions)
 
     def solve_part_2(self):
-        input_lines = self.input_data
-        print(input_lines)
-        generation = {
-            (x, y): parse_map.get(char)
-            for y, line in enumerate(input_lines, -1)
-            for x, char in enumerate(line, -1)
-            if not char == "#"
-        }
-        # print(generation)
+        _, height, width = self.input_data
+        start_pos = (0, 1)
+        end_pos = (height - 1, width - 2)
+        positions = [(start_pos, end_pos), (end_pos, start_pos), (start_pos, end_pos)]
+        return self.simulate_move(positions)
 
-        width = len(input_lines[0][1:-1])
-        height = len(input_lines[1:-1])
-        # print(width, height)
-        start_pos = (0, -1)
-        end_pos = (width - 1, height)
+    def simulate_move(self, positions):
+        valley_map, height, width = self.input_data
+        walls = {c for c, t in valley_map.items() if t == Tile.WALL}
+        inner_area_lcm = lcm(height - 2, width - 2)  # without walls
+        blizzards_maps = self.generate_blizzards_maps(
+            valley_map, inner_area_lcm, height, width
+        )
+        t = 0
+        for start_pos, end_pos in positions:
+            t = self.find_shortest_path(
+                blizzards_maps,
+                walls,
+                height,
+                width,
+                inner_area_lcm,
+                start_pos,
+                end_pos,
+                t,
+            )
+        return t
 
-        generation[start_pos] = STATE_ELF
-        generation[end_pos] = 0
+    # generates full map in each timestamp
+    # crucial observation is that layouts are cyclic so there is no need to generate each map per time unit, just all maps per cycle
+    def generate_blizzards_maps(self, valley_map, inner_area_lcm, height, width):
+        cache = {}  # blizzard positions per time
+        blizzards = [
+            (c, t[1]) for c, t in valley_map.items() if t != Tile.WALL
+        ]  # initial state
+        init_blizzards_coords = set(map(lambda b: b[0], blizzards))
+        cache[0] = init_blizzards_coords
+        for t in range(1, inner_area_lcm + 1):
+            upd_blizzards = []
+            for coord, direction in blizzards:
+                upd_coord = self.update_coord(coord, direction)
+                match direction:
+                    case Direction.UP:
+                        if upd_coord[0] == 0:
+                            upd_coord[0] = height - 2
+                    case Direction.RIGHT:
+                        if upd_coord[1] == width - 1:
+                            upd_coord[1] = 1
+                    case Direction.DOWN:
+                        if upd_coord[0] == height - 1:
+                            upd_coord[0] = 1
+                    case Direction.LEFT:
+                        if upd_coord[1] == 0:
+                            upd_coord[1] = width - 2
+                upd_blizzards.append((tuple(upd_coord), direction))
+            blizzards = upd_blizzards
+            cache[t] = set(map(lambda b: b[0], blizzards))
+        return cache
 
-        gen_count = 0
-        while True:
-            # print(gen_count)
-            # draw_scene(generation, (width, height))
-            if generation.get(end_pos) == STATE_ELF:
-                break
-            generation = new_generation(generation, (width, height), start_pos, end_pos)
-            gen_count += 1
-        generation[start_pos] = 0
-        generation[end_pos] = STATE_ELF
-        while True:
-            # draw_scene(generation, (width, height))
-            if generation.get(start_pos) == STATE_ELF:
-                break
-            generation = new_generation(generation, (width, height), end_pos, start_pos)
-            gen_count += 1
-        generation[start_pos] = STATE_ELF
-        generation[end_pos] = 0
-        while True:
-            # print(gen_count)
-            # draw_scene(generation, (width, height))
-            if generation.get(end_pos) == STATE_ELF:
-                break
-            generation = new_generation(generation, (width, height), start_pos, end_pos)
-            gen_count += 1
-        return gen_count
+    def update_coord(self, coord, direction):
+        y, x = coord
+        match direction:
+            case Direction.UP:
+                return [y - 1, x]
+            case Direction.RIGHT:
+                return [y, x + 1]
+            case Direction.DOWN:
+                return [y + 1, x]
+            case Direction.LEFT:
+                return [y, x - 1]
+
+    def find_shortest_path(
+        self,
+        blizzards_maps,
+        walls,
+        height,
+        width,
+        inner_area_lcm,
+        start_pos,
+        end_pos,
+        start_time,
+    ):
+        pq = [(start_time, start_pos)]
+        heapq.heapify(pq)
+        visited = set((start_time, start_pos))
+        while pq:
+            time, pos = heapq.heappop(pq)
+            if pos == end_pos:
+                return time
+            upd_time = time + 1
+            blizzards = blizzards_maps[upd_time % inner_area_lcm]
+            possible_moves = [pos] + list(self.compute_neighbours(pos, height, width))
+            not_occupied = list(
+                filter(lambda c: c not in walls and c not in blizzards, possible_moves)
+            )
+            for n_pos in not_occupied:
+                if (upd_time, n_pos) not in visited:
+                    heapq.heappush(pq, (upd_time, n_pos))
+                    visited.add((upd_time, n_pos))
+        return -1
+
+    def compute_neighbours(self, pos, height, width):
+        y, x = pos
+        return filter(
+            lambda pos: 0 <= pos[0]
+            and pos[0] < height
+            and 0 <= pos[1]
+            and pos[1] < width,
+            [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)],
+        )
--- days_solvers/day_24/test_day_24_solver.py
+++ days_solvers/day_24/test_day_24_solver.py
@@ -1,18 +1,57 @@
 import unittest
-from .day_24_solver import Day24Solver
+from .day_24_solver import Day24Solver, Tile, Direction
 
 
 class Day24SolverTestCase(unittest.TestCase):
     def setUp(self):
         self.solver = Day24Solver()
-        mock_input_data = [
-            "#.######",
-            "#>>.<^<#",
-            "#.<..<<#",
-            "#>v.><>#",
-            "#<^v^^>#",
-            "######.#",
-        ]
+        mock_input_data = (
+            {
+                (0, 0): Tile.WALL,
+                (0, 2): Tile.WALL,
+                (0, 3): Tile.WALL,
+                (0, 4): Tile.WALL,
+                (0, 5): Tile.WALL,
+                (0, 6): Tile.WALL,
+                (0, 7): Tile.WALL,
+                (1, 0): Tile.WALL,
+                (1, 1): (Tile.BLIZZARD, Direction.RIGHT),
+                (1, 2): (Tile.BLIZZARD, Direction.RIGHT),
+                (1, 4): (Tile.BLIZZARD, Direction.LEFT),
+                (1, 5): (Tile.BLIZZARD, Direction.UP),
+                (1, 6): (Tile.BLIZZARD, Direction.LEFT),
+                (1, 7): Tile.WALL,
+                (2, 0): Tile.WALL,
+                (2, 2): (Tile.BLIZZARD, Direction.LEFT),
+                (2, 5): (Tile.BLIZZARD, Direction.LEFT),
+                (2, 6): (Tile.BLIZZARD, Direction.LEFT),
+                (2, 7): Tile.WALL,
+                (3, 0): Tile.WALL,
+                (3, 1): (Tile.BLIZZARD, Direction.RIGHT),
+                (3, 2): (Tile.BLIZZARD, Direction.DOWN),
+                (3, 4): (Tile.BLIZZARD, Direction.RIGHT),
+                (3, 5): (Tile.BLIZZARD, Direction.LEFT),
+                (3, 6): (Tile.BLIZZARD, Direction.RIGHT),
+                (3, 7): Tile.WALL,
+                (4, 0): Tile.WALL,
+                (4, 1): (Tile.BLIZZARD, Direction.LEFT),
+                (4, 2): (Tile.BLIZZARD, Direction.UP),
+                (4, 3): (Tile.BLIZZARD, Direction.DOWN),
+                (4, 4): (Tile.BLIZZARD, Direction.UP),
+                (4, 5): (Tile.BLIZZARD, Direction.UP),
+                (4, 6): (Tile.BLIZZARD, Direction.RIGHT),
+                (4, 7): Tile.WALL,
+                (5, 0): Tile.WALL,
+                (5, 1): Tile.WALL,
+                (5, 2): Tile.WALL,
+                (5, 3): Tile.WALL,
+                (5, 4): Tile.WALL,
+                (5, 5): Tile.WALL,
+                (5, 7): Tile.WALL,
+            },
+            6,
+            8,
+        )
         self.solver.load_input = lambda: mock_input_data
 
     def tearDown(self):
@@ -20,3 +59,6 @@ class Day24SolverTestCase(unittest.TestCase):
 
     def test_solve_part_1(self):
         assert self.solver.solve(part=1)["val"] == 18
+
+    def test_solve_part_2(self):
+        assert self.solver.solve(part=2)["val"] == 54
